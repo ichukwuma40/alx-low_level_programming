dcd425bb65c64ea1abe62c191172465151dddc26
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-04-20 14:06:43 -0700

    initial commit 0. 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98.c -updates an integer value to 98
+ * @n: A pointer to an int that will be updated
+ *
+ * Return: void that means is correct.
+ */
+ void 0-reset_to_98(int *n)
+{
+ *n = 98;
+}
