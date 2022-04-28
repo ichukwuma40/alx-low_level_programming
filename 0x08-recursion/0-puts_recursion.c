fb638db21033d02596c3da0d81b20ab7c2a41a7c
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-04-28 19:00:39 +0000

    This function prints a string with recrusion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    _puts_recursion(char *s)
+
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
+	_putchar(s[0]);
 	_puts_recursion(s + 1);
 }
