6ad9fcb0c9f9c0bfc057321b8b1611b02dcb24b3
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-05-11 19:00:36 +0000

    update Macro

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,4 +3,4 @@
 
 #define PI 3.14159265359
 
-#endif /*_pi_*/
+#endif /*_PI_*/
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int i;
-    int j;
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
-}
+#endif
