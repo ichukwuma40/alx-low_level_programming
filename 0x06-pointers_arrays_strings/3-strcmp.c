11c2c27b7ec4adfada954224cc3d820476995200
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-06-29 07:23:51 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,47 +1,23 @@
 #include "main.h"
-
 /**
- *infinite_add - add two numbers.
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- *@n1: first number.
- *@n2: second number.
- *@r: result.
- *@size_r: result size.
- *Return: the addition of n1 and n2.
+ * Return: s1[i] - s2[i]
  */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int _strcmp(char *s1, char *s2)
 {
-	int add = 0, len1, len2, i, j;
+	int i;
 
-	for (len1 = 0; n1[len1]; len1++)
-	;
-	for (len2 = 0; n2[len2]; len2++)
-	;
-	if (len1 > size_r || len2 > size_r)
-		return (0);
-	len1--;
-	len2--;
-	size_r--;
-	for (i = 0; i < size_r; i++, len1--, len2--)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (len1 >= 0)
-			add += n1[len1] - '0';
-		if (len2 >= 0)
-			add += n2[len2] - '0';
-		if (len1 < 0 && len2 < 0 && add == 0)
-			break;
-		r[i] = add % 10 + '0';
-		add /= 10;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	r[i] = '\0';
-	if (len1 >= 0 || len2 >= 0 || add)
-		return (0);
-	for (i--, j = 0; i > j; i--, j++)
-	{
-		add = r[i];
-		r[i] = r[j];
-		r[j] = add;
-	}
-	return (r);
+	return (0);
 }
