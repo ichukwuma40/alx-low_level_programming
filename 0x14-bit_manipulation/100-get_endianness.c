39701f58d40c4bed2837fbb8c173dba3c0c2fb91
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-08-03 17:40:15 -0700

    bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - checks whether a machine is big endian or little
+ * get_endianness -to checks whether a machine is big endian or little
  *
  * Return: 1 if little endian, 0 otherwise
  */
