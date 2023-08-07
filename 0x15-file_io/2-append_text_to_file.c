f66296237566471a91b4bf394457d44ce30741ca
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-08-07 13:12:44 -0700

    File descriptors and permissions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,30 @@
 #include"main.h"
 
 /**
- *append_text_to_file - appends text at the end of a file
- *@filename: A pointer to the name of the file.
- *@text_content: The string to add to the end of the file.
+ *append_text_to_file - The file appends text at the end of a file
+ *@filename: This is a pointer to the name of the file.
+ *@text_content: The string adds to add to the end of the file.
  *
- *Return: If the function fails or filename is NULL -1
- *	if the file does not exist the user lacks write permissions -1
+ *Return: This if the function fails or filename is NULL -1
+ *	and if the file does not exist the user lacks write permissions -1
  *	otherwise 1
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, w, rang = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (rang = 0; text_content[rang];)
+			rang++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(o, text_content, rang);
 
 	if (o == -1 || w == -1)
 		return (-1);
