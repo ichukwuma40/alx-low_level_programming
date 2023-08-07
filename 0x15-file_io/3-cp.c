6d72aa30ed3bd5ba21474d5110cd050195a26821
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-08-07 13:23:30 -0700

    File descriptors and permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - This buffer allocates 1024 bytes for a buffer.
+ * @file: This is the name of the file buffer is storing chars for.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: This returns a pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
